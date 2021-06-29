41f4cc40d4f03cb326686f355ff7d7443434c9d3
erae22 <edward.m.rae@gmail.com>
2021-06-29 13:21:46 +0000

    A first cut at templates

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return 'The Open Championship Pool'
+@app.route("/")
+def index():
+    return render_template("main_page.html")
+
+@app.route('/wibble')
+def wibble():
+    return 'Wibble Woo'
 
